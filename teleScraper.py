6c97997eedfdc8abb8b5275db6cc8255a30814c2
diamongwolfcode <amitfilber1@gmail.com>
2023-10-12 12:06:33 +0300

    now you can put multipule channels

--- teleScraper.py
+++ teleScraper.py
@@ -4,17 +4,19 @@ from telethon.sync import TelegramClient
 name = NAME
 api_id = ID # int
 api_hash = HASH
-chat = CHANNEL_LINK
 
-data = [] # stores all our data in the format SENDER_ID, MSG
-
-
-with TelegramClient(name, api_id, api_hash) as client:
-    for message in client.iter_messages(chat):
-        data.append([message.sender_id, message.text])
+channels = [] # add chat links here
 
 
-df = pd.DataFrame(data, columns=['SENDER', 'MESSAGE']) # creates a new dataframe
+for chat in channels:
+    data = [] # stores all our data in the format SENDER_ID, MSG
+
+    with TelegramClient(name, api_id, api_hash) as client:
+        for message in client.iter_messages(chat):
+            data.append([message.sender_id, message.text])
 
 
-df.to_csv('test.csv', encoding='utf-8') # save to a CSV file
+    df = pd.DataFrame(data, columns=['SENDER', 'MESSAGE']) # creates a new dataframe
+
+
+    df.to_csv(f'{chat}.csv', encoding='utf-8') # save to a CSV file
