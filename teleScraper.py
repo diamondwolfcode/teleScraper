d3ce174f8a88eaacfb0b4c12b3cd0e1f1aecb36c
diamongwolfcode <amitfilber1@gmail.com>
2023-10-12 12:26:49 +0300

    added something

--- teleScraper.py
+++ teleScraper.py
@@ -1,22 +1,23 @@
 import pandas as pd
 from telethon.sync import TelegramClient
 
-name = NAME
-api_id = ID # int
+name = NAME 
+api_id = ID
 api_hash = HASH
 
 channels = [] # add chat links here
 
-
+i = 0
 for chat in channels:
+    i += 1
     data = [] # stores all our data in the format SENDER_ID, MSG
 
     with TelegramClient(name, api_id, api_hash) as client:
         for message in client.iter_messages(chat):
-            data.append([message.sender_id, message.text, message.id])
+            data.append([message.sender_id, message.text, message.id, message.get_sender()])
 
 
-    df = pd.DataFrame(data, columns=['SENDER', 'MESSAGE']) # creates a new dataframe
+    df = pd.DataFrame(data, columns=['SENDER', 'MESSAGE', 'ID', 'NAME']) # creates a new dataframe
 
 
-    df.to_csv(f'{chat}.csv', encoding='utf-8') # save to a CSV file
+    df.to_csv(f'chat{i}.csv', encoding='utf-8') # save to a CSV file
